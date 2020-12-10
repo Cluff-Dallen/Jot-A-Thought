f31f798bcab1e7034f672ed6d5cd234d2b8ab3f6
Dallen R. Cluff <49734187+Cluff-Dallen@users.noreply.github.com>
2020-12-10 11:54:23 -0600

    Update SplashScreen.java
    Added a comment, small coding format/clean up changes made.

--- app/src/main/java/group06/com/jot_a_thought/ui/activity/SplashScreen.java
+++ app/src/main/java/group06/com/jot_a_thought/ui/activity/SplashScreen.java
@@ -1,30 +1,27 @@
 package group06.com.jot_a_thought.ui.activity;
 
+//Imports
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
 import group06.com.jot_a_thought.R;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
         Handler handle = new Handler();
         handle.postDelayed(new Runnable() {
             @Override
-            public void run() {
+            public void run(){
                 Intent intent = new Intent(SplashScreen.this, PinScreen.class);
                 startActivity(intent);
                 finish();
             }
         }, 3000);
-
-
     }
 }
