b4f0eb22c3d68c98ddd6e79504cda07baf305ad7
Dallen R. Cluff <49734187+Cluff-Dallen@users.noreply.github.com>
2020-12-10 11:52:19 -0600

    Update PinScreen.java
    Added a few comments, small coding format/clean up changes made.

--- app/src/main/java/group06/com/jot_a_thought/ui/activity/PinScreen.java
+++ app/src/main/java/group06/com/jot_a_thought/ui/activity/PinScreen.java
@@ -1,5 +1,6 @@
 package group06.com.jot_a_thought.ui.activity;
 
+//Imports
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -11,17 +12,14 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
 import group06.com.jot_a_thought.R;
 
 public class PinScreen extends AppCompatActivity{
@@ -32,15 +30,14 @@ public class PinScreen extends AppCompatActivity{
     int attempts;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pin_journal);
 
-        // block of code to change actionBar color
+        //Block of code to change actionBar color
         ActionBar actionBar;
         actionBar = getSupportActionBar();
-        ColorDrawable colorDrawable
-                = new ColorDrawable(Color.parseColor("#3F51B5"));
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#3F51B5"));
         actionBar.setBackgroundDrawable(colorDrawable);
 
         System.out.println("pin Screen");
@@ -50,9 +47,10 @@ public class PinScreen extends AppCompatActivity{
         super.onResume();
         attempts = 0;
         FileReader fr = null;
+        
         try {
             fr = new FileReader(new File(getExternalFilesDir(null), "PIN"));
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e){
             TextView screenTitle = findViewById(R.id.pinTitle);
             screenTitle.setText(getString(R.string.createPIN));
             return;
@@ -62,39 +60,46 @@ public class PinScreen extends AppCompatActivity{
             BufferedReader br = new BufferedReader(fr);
             String line = "";
 
-            while (true) {
+            while (true){
                 try {
                     if (!((line = br.readLine()) != null)) break;
-                } catch (IOException e) {
+                } catch (IOException e){
                     e.printStackTrace();
                 }
                 checkPin += line;
             }
             br.close();
-        } catch (IOException e) {
+        } catch (IOException e){
             e.printStackTrace();
         }
+        
         System.out.println("Check Pin: " + checkPin);
-
         TextView screenTitle = findViewById(R.id.pinTitle);
+        
         if(checkPin.equals("")){
             screenTitle.setText(getString(R.string.newPIN));
         }
+        
         else{
             screenTitle.setText(getString(R.string.enterPIN));
         }
     }
 
-    private void checkPin() {
+    //Check to make sure pin is correct legnth and only allow a set number of attempts
+    private void checkPin(){
         System.out.println(pin);
-        if (pin.length() == 4 && attempts <= ATTEMPT_LIMIT) {
+        if (pin.length() == 4 && attempts <= ATTEMPT_LIMIT){
             attempts += 1;
             System.out.println(attempts);
-            if (pin.equals(checkPin)) {
+            
+            //If pin is correct
+            if (pin.equals(checkPin)){
                 Intent intent = new Intent(PinScreen.this, JournalList.class);
                 startActivity(intent);
                 finish();
-            } else if (checkPin.equals("")) {
+            }
+            
+            else if (checkPin.equals("")){
                 File file = new File(getExternalFilesDir(null), "PIN");
 
 
@@ -112,7 +117,10 @@ public class PinScreen extends AppCompatActivity{
                 Intent intent = new Intent(PinScreen.this, JournalList.class);
                 startActivity(intent);
                 finish();
-            } else {
+            }
+            
+            //If max attemps hasn't been met, allow another attempt 
+            else {
                 if (attempts <= ATTEMPT_LIMIT) {
                     Toast toast = Toast.makeText(getApplicationContext(),
                             "PIN Incorrect\nAttempts Left: " + (ATTEMPT_LIMIT - (attempts - 1)),
@@ -123,6 +131,8 @@ public class PinScreen extends AppCompatActivity{
                 }
             }
         }
+        
+        //If pin is incorrect and exceeds the number of allowed attempts
         if(attempts > ATTEMPT_LIMIT){
             Toast toast = Toast.makeText(getApplicationContext(),
                     "PIN Incorrect\nClosing Application",
@@ -132,9 +142,8 @@ public class PinScreen extends AppCompatActivity{
         }
     }
 
-    public <View> void pinButton(android.view.View v) {
+    public <View> void pinButton(android.view.View v){
         pin += v.getTag().toString();
         checkPin();
     }
-
 }
