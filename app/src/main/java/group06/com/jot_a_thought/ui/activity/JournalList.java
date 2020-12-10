6f378c581799652ed7dd19505adf422fa37b681c
Dallen R. Cluff <49734187+Cluff-Dallen@users.noreply.github.com>
2020-12-10 12:02:19 -0600

    Update JournalList.java
    Added a few comments, small coding format/clean up changes made.

--- app/src/main/java/group06/com/jot_a_thought/ui/activity/JournalList.java
+++ app/src/main/java/group06/com/jot_a_thought/ui/activity/JournalList.java
@@ -1,5 +1,6 @@
 package group06.com.jot_a_thought.ui.activity;
 
+//Imports
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -13,62 +14,60 @@ import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.ToggleButton;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
 import group06.com.jot_a_thought.R;
 import group06.com.jot_a_thought.dao.JournalDAO;
 
-
-//testing
-public class JournalList extends AppCompatActivity {
+public class JournalList extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_journal_list);
 
-        // block of code to change actionBar color
+        //Block of code to change actionBar color
         ActionBar actionBar;
         actionBar = getSupportActionBar();
-        ColorDrawable colorDrawable
-                = new ColorDrawable(Color.parseColor("#3F51B5"));
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#3F51B5"));
         actionBar.setBackgroundDrawable(colorDrawable);
 
         FloatingActionButton buttonNewJournal = findViewById(R.id.activity_journal_list_fab_new_journal);
-        buttonNewJournal.setOnClickListener(new View.OnClickListener() {
+        buttonNewJournal.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 startActivity(new Intent(JournalList.this, NewJournalEntryActivity.class));
             }
         });
     }
+    
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.activity_journal_list_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+      
         int itemId = item.getItemId();
-        if (itemId == R.id.activity_journal_list_menu_dark_mode) {
+        
+        if (itemId == R.id.activity_journal_list_menu_dark_mode){
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         }
-        if (itemId == R.id.activity_journal_list_menu_white_mode) {
+        
+        if (itemId == R.id.activity_journal_list_menu_white_mode){
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
-        if (itemId == R.id.activity_journal_list_menu_change_PIN) {
+        
+        if (itemId == R.id.activity_journal_list_menu_change_PIN){
 
             File file = new File(getExternalFilesDir(null),"PIN");
 
@@ -78,7 +77,7 @@ public class JournalList extends AppCompatActivity {
                 writer.flush();
                 writer.close();
                 Log.i("Saved in PIN", "PIN Saved");
-            } catch (IOException f) {
+            } catch (IOException f){
                 Log.i("Saving in PIN", "PIN saved");
                 f.printStackTrace();
             }
@@ -87,27 +86,22 @@ public class JournalList extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume(){
+       
         super.onResume();
-
         JournalDAO dao = new JournalDAO();
-
         String path = getExternalFilesDir(null).toString();
-
         ListView journalEntries = findViewById(R.id.activity_journal_list_listview);
 
-        journalEntries.setAdapter(new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1,
-                dao.allJournals(path)));
+        journalEntries.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.allJournals(path)));
 
-        journalEntries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        journalEntries.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
                 String selectedJournal = (String) parent.getItemAtPosition(position);
                 String title = selectedJournal.split("\n")[0];
                 Log.i("On ListView click", title);
@@ -116,6 +110,5 @@ public class JournalList extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
 }
